a0e00001ce1c08b92b0e2e799c584e1db7fe8887
Sneha P Pratap <snehapratap248@gmail.com>
2025-04-18 08:37:07 +0530

    done

---
+++ app/resources.py
@@ -0,0 +1,8 @@
+mental_health_links = {
+    "WHO": "https://www.who.int/mental_health/en/",
+    "India": "https://mentalhealthindia.org/helplines",
+    "Global Crisis": "https://checkpointorg.com/global/"
+}
+
+def get_resources():
+    return mental_health_links
